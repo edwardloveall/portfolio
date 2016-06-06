494994d434e00529a01f05b9943c427448be4046
Edward Loveall <edward@edwardloveall.com>
2016-06-06 14:53:21 -0400

    Add music index

--- spec/features/user_visits_songs_spec.rb
+++ spec/features/user_visits_songs_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature 'User visits songs' do
+  scenario 'sees list of songs' do
+    song = create(:song)
+
+    visit '/'
+    click_on('Music')
+
+    expect(page).to have_link(song.title, href: song_path(song))
+  end
+
   scenario 'user sees the song page' do
     song = create(:song)
 
-    visit music_path(song)
+    visit song_path(song)
 
     expect(page).to have_content(song.title)
     expect(page).to have_content(song.description)
