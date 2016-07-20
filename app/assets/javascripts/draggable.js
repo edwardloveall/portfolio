d6588d8c4fcb1a44ffe38ea085fbd90c0a1e54ea
Edward Loveall <edward@edwardloveall.com>
2016-07-19 21:41:11 -0400

    Refactor sortable selector

--- app/assets/javascripts/draggable.js
+++ app/assets/javascripts/draggable.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('body.admin-projects ul.projects, body.admin-songs ul.songs').sortable({
+  $('ul.sortable').sortable({
     axis: 'y',
     update: updatePositions
   })
