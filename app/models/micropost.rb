58bc48993acf3b76b84079ab04594ddfdf642e02
Edward Loveall <edward@edwardloveall.com>
2016-11-15 12:28:28 -0500

    Add guid to microposts
    This is very similar to the post's guid but uses a timestamp instead of
    a slug because microposts have no slug. The guids for microposts look
    like:
    
    'com.edwardloveall.microblog.1479231376'
    
    As I said in the post guid commit (c1f2de1):
    
    This hierarchical notation works well for apps, so I thought it may work
    well for post guids. Each post's slug is both required and unique.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,7 @@
 class Micropost < ApplicationRecord
   validates :body, presence: true, length: { maximum: 280 }
+
+  def guid
+    "com.edwardloveall.microblog.#{created_at.to_i}"
+  end
 end
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Micropost do
     it { should validate_presence_of(:body) }
     it { should validate_length_of(:body).is_at_most(280) }
   end
+
+  describe '#guid' do
+    it 'returns the guid for the micropost' do
+      micropost = create(:micropost)
+      guid = "com.edwardloveall.microblog.#{micropost.created_at.to_i}"
+
+      expect(micropost.guid). to eq(guid)
+    end
+  end
 end
