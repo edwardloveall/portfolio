40a1485c1ae239e0eaf00d60e6cab553964fcbf4
Edward Loveall <edward@edwardloveall.com>
2018-04-08 14:48:26 -0400

    Remove pid check from puma config
    This was me trying to second-guess puma's logic and it was pretty
    annoying. I'm not sure how puma handles an existing PID file by
    default, but I probably shouldn't implement that myself.

--- config/puma.rb
+++ config/puma.rb
@@ -4,15 +4,6 @@ app_dir = File.expand_path("../..", __FILE__)
 shared_dir = File.expand_path("#{app_dir}/shared", __FILE__)
 pid_path = "#{shared_dir}/pids/puma.pid"
 rails_env = ENV["RAILS_ENV"] || "production"
-
-if File.exists?(pid_path) && ARGF.argv.include?("start")
-  puts "-" * 20
-  puts "Current server running with PID: #{File.read(pid_path)}"
-  puts "Stop current server with: pumactl stop"
-  puts "-" * 20
-  exit 1
-end
-
 threads 1, 6
 environment rails_env
 bind "unix://#{shared_dir}/sockets/puma.sock"
