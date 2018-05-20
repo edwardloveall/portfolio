d2243eb7b470bad3b763be8daad83a4302a8086f
Edward Loveall <edward@edwardloveall.com>
2018-05-19 21:42:27 -0400

    Include query params when redirecting after sign in
    The trick was using request.original_fullpath all along!

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -5,7 +5,7 @@ class AuthenticatedController < ApplicationController
   def require_login
     if !signed_in?
       flash.notice = Monban.config.sign_in_notice.call
-      session[:original_path] = url_for(params.to_h.merge(only_path: true))
+      session[:original_path] = request.original_fullpath
 
       redirect_to Monban.config.no_login_redirect
     end
--- spec/features/admin_redirect_spec.rb
+++ spec/features/admin_redirect_spec.rb
@@ -11,4 +11,17 @@ RSpec.describe "Admin redirect" do
 
     expect(current_url).to eq(new_admin_micropost_url)
   end
+
+  context "when there are query parameters" do
+    it "redirects to that path including those parameters" do
+      user = create(:user)
+
+      visit new_admin_micropost_path(foo: "bar")
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password_digest
+      click_on "Sign in"
+
+      expect(current_url).to eq(new_admin_micropost_url(foo: "bar"))
+    end
+  end
 end
