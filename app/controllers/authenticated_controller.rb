6ca3788b0265cb5e2e0108682d5b853d29c2eddd
Edward Loveall <edward@edwardloveall.com>
2018-05-19 21:33:44 -0400

    Create a middle Authenticated controller
    This is for places in the app that need a user to be logged in but
    aren't the admin section, such as adding a new 3rd party Authorization

---
+++ app/controllers/authenticated_controller.rb
@@ -0,0 +1,13 @@
+class AuthenticatedController < ApplicationController
+  include Monban::ControllerHelpers
+  before_action :require_login
+
+  def require_login
+    if !signed_in?
+      flash.notice = Monban.config.sign_in_notice.call
+      session[:original_path] = url_for(params.to_h.merge(only_path: true))
+
+      redirect_to Monban.config.no_login_redirect
+    end
+  end
+end
