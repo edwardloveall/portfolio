d318b0ac2a41357a48f47ff32ccadae8de350668
Edward Loveall <edward@edwardloveall.com>
2018-06-20 11:39:08 -0400

    Copy attachments to activestorage from paperclip
    This migration is from here:
    https://github.com/thoughtbot/paperclip/blob/master/MIGRATING.md#copy-th
    e-database-data-over
    
    I had to modify it a bit from the source because prepared statements
    didn't seem to work? I wonder if that's because the PG gem went to 1.0
    recently. Either way, I used `exec_params` instead of `prepare` which
    seemed to work fine. I also replaced the generic placeholder arguments
    `?` with numbered parameters `$1`, `$2`, etc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_19_190314) do
+ActiveRecord::Schema.define(version: 2018_06_19_203659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
