75ed43ce266a32167e6807da1ba5ac1b7867d1f7
Edward Loveall <edward@edwardloveall.com>
2018-08-02 22:06:05 -0400

    Install bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,4 +16,9 @@ Rails.application.configure do
   config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.middleware.use RangeFilter
+  config.after_initialize do
+    Bullet.add_footer = true
+    Bullet.enable = true
+    Bullet.rails_logger = true
+  end
 end
