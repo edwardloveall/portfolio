801b631520964251976dbff34e2999d63a506d30
Edward Loveall <edward@edwardloveall.com>
2019-09-29 16:26:30 -0400

    Add config.hosts to production.rb and alphabetize

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,25 +1,26 @@
 require Rails.root.join('config/smtp')
 Rails.application.configure do
-  config.cache_classes = true
-  config.eager_load = true
-  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.middleware.use Rack::Deflater
-  config.assets.js_compressor = :uglifier
-  config.assets.compile = false
-  config.assets.digest = true
-  config.log_level = :debug
   config.action_controller.asset_host = ENV.fetch('ASSET_HOST',
                                                   ENV.fetch('APPLICATION_HOST'))
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = SMTP_SETTINGS
-  config.i18n.fallbacks = true
-  config.active_support.deprecation = :notify
-  config.log_formatter = ::Logger::Formatter.new
-  config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = {
-    host: ENV.fetch('APPLICATION_HOST')
+    host: ENV.fetch('APPLICATION_HOST'),
   }
+  config.action_mailer.smtp_settings = SMTP_SETTINGS
+  config.active_record.dump_schema_after_migration = false
   config.active_storage.service = :local
+  config.active_support.deprecation = :notify
+  config.assets.compile = false
+  config.assets.digest = true
+  config.assets.js_compressor = :uglifier
+  config.cache_classes = true
+  config.consider_all_requests_local = false
+  config.eager_load = true
+  config.hosts << ENV.fetch('APPLICATION_HOST')
+  config.i18n.fallbacks = true
+  config.log_formatter = ::Logger::Formatter.new
+  config.log_level = :debug
+  config.middleware.use Rack::Deflater
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 end
