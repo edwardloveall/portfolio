6dfa2dfbc98bf45aee46e3382d5f4c14eeec3b43
Edward Loveall <edward@edwardloveall.com>
2019-11-17 15:29:18 -0500

    Add asset manifest

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,10 @@
+//= link_tree ../images
+//= link_tree ../videos
+
+//= link_directory ../javascripts .js
+
+//= link admin/admin.css
+//= link application.css
+//= link blog.css
+//= link microblog.css
+//= link_directory ../stylesheets .css
