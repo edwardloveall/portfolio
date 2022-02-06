8c7565ed08f339cbf3719014c7be7fdbed8e8e28
Edward Loveall <edward@edwardloveall.com>
2022-02-06 17:22:30 -0500

    remove deprecated babel config

--- babel.config.js
+++ babel.config.js
@@ -11,7 +11,7 @@ module.exports = function (api) {
         '`BABEL_ENV` environment variables. Valid values are "development", ' +
         '"test", and "production". Instead, received: ' +
         JSON.stringify(currentEnv) +
-        ".",
+        "."
     );
   }
 
@@ -41,12 +41,6 @@ module.exports = function (api) {
       "@babel/plugin-syntax-dynamic-import",
       isTestEnv && "babel-plugin-dynamic-import-node",
       "@babel/plugin-transform-destructuring",
-      [
-        "@babel/plugin-proposal-class-properties",
-        {
-          loose: true,
-        },
-      ],
       [
         "@babel/plugin-proposal-object-rest-spread",
         {
