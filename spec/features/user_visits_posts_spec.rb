36580bbcf13acde2457e677229a79460ba48b4c0
Edward Loveall <edward@edwardloveall.com>
2022-03-26 13:04:36 -0400

    Update user post spec
    Adding a test for markdown and also updating it to match modern style
    prefs

--- spec/features/user_visits_posts_spec.rb
+++ spec/features/user_visits_posts_spec.rb
@@ -1,41 +1,51 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.feature 'User visits posts' do
-  scenario 'and sees pagination links' do
+feature "User visits posts" do
+  scenario "and sees next page pagination links" do
     create_list(:post, 11)
 
-    visit root_url(subdomain: 'blog')
+    visit root_url(subdomain: "blog")
 
-    expect(page).to have_link('Older', href: root_path(page: 2))
+    expect(page).to have_link("Older", href: root_path(page: 2))
   end
 
-  scenario 'and sees pagination links' do
+  scenario "and sees previous page pagination links" do
     create_list(:post, 11)
 
-    visit root_url(subdomain: 'blog', page: 2)
+    visit root_url(subdomain: "blog", page: 2)
 
-    expect(page).to have_link('Newer', href: root_path)
+    expect(page).to have_link("Newer", href: root_path)
   end
 
-  scenario "and visits a specific post" do
-    internal_post = create(:internal_post, title: "Title")
+  scenario "can visit main site from footer" do
+    visit root_url(subdomain: "blog")
+
+    within("footer") do
+      expect(page).to have_link(
+        "Edward Loveall",
+        href: "https://edwardloveall.com",
+      )
+    end
+  end
+end
+
+feature "User visits a specific post" do
+  scenario "and sees a post rendered with markdown" do
+    internal_post = create(
+      :internal_post,
+      title: "Title",
+      body: "_hello_",
+      slug: "title"
+    )
     post = create(:post, postable: internal_post)
 
     visit root_url(subdomain: "blog")
     click_on("Title")
 
     expect(current_url).to eq(
-      internal_post_url(subdomain: "blog", slug: internal_post.slug)
+      internal_post_url(subdomain: "blog", slug: "title")
     )
     expect(page).to have_selector("article h2", text: "Title")
-  end
-
-  scenario 'can visit main site from footer' do
-    visit root_url(subdomain: 'blog')
-
-    within('footer') do
-      expect(page).to have_link('Edward Loveall',
-                                href: 'https://edwardloveall.com')
-    end
+    expect(page).to have_selector("article em", text: "hello")
   end
 end
