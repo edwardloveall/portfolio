6ae04b28304edb3bd1f8b3fcacbece37e83b4d75
Edward Loveall <edward@edwardloveall.com>
2023-01-02 14:57:05 -0500

    Add stimulus

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 import "../assets/javascripts/detector";
 import "../assets/javascripts/scrollDown";
 import "../assets/javascripts/song";
+
+import { Application } from "@hotwired/stimulus";
+
+import HelloController from "./controllers/hello_controller";
+
+window.Stimulus = Application.start();
+Stimulus.register("hello", HelloController);
