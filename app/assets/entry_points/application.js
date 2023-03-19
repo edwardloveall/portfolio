54881fa8b381e9f0ac06348ca7989c599d00140e
Edward Loveall <edward@edwardloveall.com>
2023-03-19 16:41:07 -0400

    Use default audio player
    It's both more accessible and uses less code.
    
    Oddly, the audio element adds some weird bottom margin unless the property is
    `display: block`
    
    https://css-tricks.com/forums/topic/solved-unusual-bottom-margin-on-html5-video-element/

--- app/assets/entry_points/application.js
+++ app/assets/entry_points/application.js
@@ -1,5 +1,4 @@
 import "../javascripts/detector";
 import "../javascripts/scrollDown";
-import "../javascripts/song";
 
 import "../stylesheets/application.scss";
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,2 @@
 import "../assets/javascripts/detector";
 import "../assets/javascripts/scrollDown";
-import "../assets/javascripts/song";
