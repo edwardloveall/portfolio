9b8d5ff23fe2ca1a615cf9a3e7a4c302c0419fb6
Edward Loveall <edward@edwardloveall.com>
2023-10-14 11:01:02 -0400

    Fully update to rails 7.1

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,9 @@ Bundler.require(*Rails.groups)
 
 module Portfolio
   class Application < Rails::Application
-    config.load_defaults 7.0
+    config.load_defaults 7.1
     config.active_storage.variant_processor = :mini_magick
     config.autoload_lib(ignore: %w[assets tasks])
+    config.active_support.cache_format_version = 7.1
   end
 end
