ae0dc4279b1d45b3880f5f032871d75de9e3ad40
Edward Loveall <edward@edwardloveall.com>
2023-11-14 20:56:52 -0500

    format Navigation

--- app/classes/navigation.rb
+++ app/classes/navigation.rb
@@ -1,21 +1,21 @@
 class Navigation < ActionView::Base
   attr_reader :path
 
-  def self.links(path = '/')
+  def self.links(path = "/")
     new(path).links
   end
 
-  def initialize(path = '/')
+  def initialize(path = "/")
     @path = path
   end
 
   def links
     content_tag(:nav, class: :main) do
-      ['about', 'projects', 'music'].each do |page|
+      ["about", "projects", "music"].each do |page|
         if active_page == page
-          concat link_to(page.titleize, "/#{page}", class: :active)
+          concat(link_to(page.titleize, "/#{page}", class: :active))
         else
-          concat link_to(page.titleize, "/#{page}")
+          concat(link_to(page.titleize, "/#{page}"))
         end
       end
     end
@@ -24,6 +24,6 @@ class Navigation < ActionView::Base
   private
 
   def active_page
-    @active ||= path.split('/')[1] || 'projects'
+    @active ||= path.split("/")[1] || "projects"
   end
 end
